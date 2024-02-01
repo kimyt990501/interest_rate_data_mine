7bac611717f99fb79a97add607bf19089ec676e7
kimyt990501 <kimyt990501@naver.com>
2024-02-01 15:57:45 +0900

    데이터 시각화

--- it_kskqku_data_mining.py
+++ it_kskqku_data_mining.py
@@ -18,5 +18,36 @@ set_matplotlib_formats('retina')
 ir_kskqku_data = pd.read_csv('C:\\Users\\user\\Desktop\\github_repo\\interest_rate_data_mine\\data\\ir_kskqku_data.csv', index_col=0 ,encoding='euc-kr') # 전체 경로 (윈도우)
 #ir_kskqku_data = pd.read_csv('interest_rate_data_mine\\data\\ir_kskqku_data.csv', index_col=0 ,encoding='euc-kr') # 상대경로
 
+# 1. 그래프 시각화
+
+# 인덱스 값 설정
+#idx = ir_data.index.astype('str')
+idx = pd.to_datetime(ir_kskqku_data.index)
+idx_year = idx.year.astype('str')
+
+# 1.1 데이터 각각 시각화
+
+# 기준 금리, 원/달러 환율 그래프 그리기 (선 그래프)
+plt.figure(figsize = (12,10))
+
+sns.lineplot(x=idx_year ,y='기준금리', data = ir_kskqku_data, label = "기준금리")
+sns.lineplot(x=idx_year ,y='원달러환율', data = ir_kskqku_data, label = "원/달러환율")
+
+plt.legend()
+sns.set_context('poster', font_scale = 1)
+
+plt.xticks(rotation=45)
+
+# 기준 금리, 주가 그래프 그리기 (선 그래프)
+plt.figure(figsize = (12,10))
+
+sns.lineplot(x=idx_year ,y='기준금리', data = ir_kskqku_data, label = "기준금리")
+sns.lineplot(x=idx_year ,y='코스피지수', data = ir_kskqku_data, label = "코스피지수")
+sns.lineplot(x=idx_year ,y='코스닥지수', data = ir_kskqku_data, label = "코스닥지수")
+
+plt.legend()
+sns.set_context('poster', font_scale = 1)
+
+plt.xticks(rotation=45)
 
 # %%
